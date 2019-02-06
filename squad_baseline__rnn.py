3b1540a1334085addba4e09e9c71881e1a9ca3ee
JacobLoe <Jcbl423@gmail.com>
2019-02-06 15:44:30 +0100

    updated baseline model,added save option for model

--- squad_baseline__rnn.py
+++ squad_baseline__rnn.py
@@ -10,6 +10,7 @@ from keras.models import Model
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
 
+################################################################
 # rnn parameters
 RNN = recurrent.LSTM
 EMBED_HIDDEN_SIZE = 50
@@ -19,15 +20,16 @@ BATCH_SIZE = 32
 EPOCHS = 100
 
 #glove embedding parameters
-BASE_DIR = ''
-GLOVE_DIR = os.path.join(BASE_DIR, '../glove')
-MAX_SEQUENCE_LENGTH = 1000
-MAX_NUM_WORDS = 20000
+# BASE_DIR = ''
+# GLOVE_DIR = os.path.join(BASE_DIR, 'glove')
+GLOVE_DIR = '../glove/glove.6B.100d.txt'
+#MAX_SEQUENCE_LENGTH = 1000
+# MAX_NUM_WORDS = 80000 #20000
 EMBEDDING_DIM = 100
-VALIDATION_SPLIT = 0.2
-
-#open SQuAD-dataset and extract the relevatn data
-print('open and extract SQuAD')
+EVAL_SPLIT = 0.2 
+#################################################################
+#open SQuAD-dataset and extract the relevant data from the json-file
+#to a easier readable/accessible dictionary
 with open('SQuAD/train-v2.0.json') as file:
     train=json.load(file)
 train_context=[]
@@ -43,8 +45,8 @@ for j,data in enumerate(train['data']):
                 train_new['context'].append(context)
                 train_new['question'].append(qas['question'])
                 train_new['answer'].append(qas['answers'][0]['text'])
-
-#concatenate the data in on vector for preprocessing
+####################################################################
+#concatenate the data in one vector for preprocessing
 train_all=[]
 for line in train_new['context']:
     train_all.append(line)
@@ -52,47 +54,58 @@ for line in train_new['question']:
     train_all.append(line)
 for line in train_new['answer']:
     train_all.append(line)
-
+####################################################################
+from keras.preprocessing.text import text_to_word_sequence
+vocab = set()
+for text in train_all:
+    vocab |= set(text_to_word_sequence(text))
+vocab = sorted(vocab)
+vocab_size = len(vocab) + 1
+print(np.shape(vocab))
+#####################################################################
 #prepare the data to use as input of the rnn
-print('prepare the input data for rnn')
-tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
+tokenizer = Tokenizer(num_words=vocab_size)
 tokenizer.fit_on_texts(train_all)
 context_sequences = tokenizer.texts_to_sequences(train_new['context'])
 question_sequences = tokenizer.texts_to_sequences(train_new['question'])
 answer_sequences = tokenizer.texts_to_sequences(train_new['answer'])
 
+max_len_context=max(map(len,context_sequences))
+max_len_question=max(map(len,question_sequences))
+max_len_answer=max(map(len,answer_sequences))
+
 word_index = tokenizer.word_index
 print('Found %s unique tokens.' % len(word_index))
 
-context = pad_sequences(context_sequences, maxlen=MAX_SEQUENCE_LENGTH)
-question = pad_sequences(question_sequences, maxlen=MAX_SEQUENCE_LENGTH)
-answer = pad_sequences(answer_sequences, maxlen=MAX_SEQUENCE_LENGTH)
+context = pad_sequences(context_sequences, maxlen=max_len_context)
+question = pad_sequences(question_sequences, maxlen=max_len_question)
+answer = pad_sequences(answer_sequences, maxlen=max_len_answer)
 
 print('Shape of context tensor:', context.shape)
 print('Shape of question tensor:', question.shape)
 print('Shape of answer tensor:', answer.shape)
-
-# split the data into a training set and a validation set
-print('create train and test set')
+##########################################################################
+# split the data into a training set and an evaluation set
 indices = np.arange(context.shape[0])
 np.random.shuffle(indices)
 context = context[indices]
 question = question[indices]
 answer = answer[indices]
-num_validation_samples = int(VALIDATION_SPLIT * context.shape[0])
+num_eval_samples = int(EVAL_SPLIT * context.shape[0])
 
-x_train_context = context[:-num_validation_samples]
-x_train_question = question[:-num_validation_samples]
-y_train_answer = answer[:-num_validation_samples]
+x_train_context = context[:-num_eval_samples]
+x_train_question = question[:-num_eval_samples]
+y_train_answer = answer[:-num_eval_samples]
 
-x_val_context = context[-num_validation_samples:]
-x_val_question = question[-num_validation_samples:]
-y_val_answer = answer[-num_validation_samples:]
-
+x_eval_context = context[-num_eval_samples:]
+x_eval_question = question[-num_eval_samples:]
+y_eval_answer = answer[-num_eval_samples:]
+##########################################################################
+#FIX_ME: add glove download
+# https://nlp.stanford.edu/projects/glove/
 #get glove embeddings
-print('get the glove embeddings')
 embeddings_index = {}
-f = open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt'))
+f = open(GLOVE_DIR)#os.path.join(GLOVE_DIR, 'glove.6B.100d.txt'))
 for line in f:
     values = line.split()
     word = values[0]
@@ -101,7 +114,7 @@ for line in f:
 f.close()
 
 print('Found %s word vectors.' % len(embeddings_index))
-
+###########################################################################
 #extract the glove-embedding to a matrix
 embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM))
 for word, i in word_index.items():
@@ -109,26 +122,33 @@ for word, i in word_index.items():
     if embedding_vector is not None:
         # words not found in embedding index will be all-zeros.
         embedding_matrix[i] = embedding_vector
-
-#create a non-trainable embedding layer
-embedding_layer = Embedding(len(word_index) + 1,
+#create non-trainable embedding layers
+# for the context and the question each
+context_embedding_layer = Embedding(len(word_index) + 1,
                             EMBEDDING_DIM,
                             weights=[embedding_matrix],
-                            input_length=MAX_SEQUENCE_LENGTH,
-                            trainable=False)
-
+                            input_length=max_len_context,
+                            trainable=False,
+                            name='context_embedding')
+question_embedding_layer = Embedding(len(word_index) + 1,
+                            EMBEDDING_DIM,
+                            weights=[embedding_matrix],
+                            input_length=max_len_question,
+                            trainable=False,
+                            name='question_embedding')
+###########################################################################
 print('Build model...')
 
-context_layer = layers.Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32',name='Context_input')
-encoded_context = embedding_layer(context_layer)
+context_layer = layers.Input(shape=(max_len_context,), dtype='int32',name='Context_input')
+encoded_context = context_embedding_layer(context_layer)
 encoded_context = RNN(SENT_HIDDEN_SIZE)(encoded_context)
 
-question_layer = layers.Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32',name='Question_input')
-encoded_question = embedding_layer(question_layer)
+question_layer = layers.Input(shape=(max_len_question,), dtype='int32',name='Question_input')
+encoded_question = question_embedding_layer(question_layer)
 encoded_question = RNN(QUERY_HIDDEN_SIZE)(encoded_question)
 
 merged = layers.concatenate([encoded_context, encoded_question])
-preds = layers.Dense(1000, activation='softmax')(merged) #dimensions of dense layer have to to the same as the answer dimensions
+preds = layers.Dense(max_len_answer, activation='softmax')(merged) #dimensions of dense layer have to to the same as the answer dimensions
 
 model = Model([context_layer, question_layer], preds)
 model.compile(optimizer='adam',
@@ -136,7 +156,7 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 print('successfully built the model')
 print(model.summary())
-
+##########################################################################
 print('Training')
 model.fit([x_train_context, x_train_question], y_train_answer,
           batch_size=BATCH_SIZE,
@@ -144,10 +164,12 @@ model.fit([x_train_context, x_train_question], y_train_answer,
           validation_split=0.05)
 
 print('Evaluation')
-loss, acc = model.evaluate([x_val_context, x_val_question], y_val_answer,
+loss, acc = model.evaluate([x_eval_context, x_eval_question], y_eval_answer,
                            batch_size=BATCH_SIZE)
 print('Test loss / test accuracy = {:.4f} / {:.4f}'.format(loss, acc))
-
-print('saving weights')
-with open('results/baseline_total_weights.vec','w') as f:
-    print(model.get_weights(),file=f)
+############################################################################
+print('save model')
+model_json = model.to_json()
+with open("models/baseline_model.json",'w') as json_file:
+    json_file.write(model_json)
+model.save_weights('model.h5')
